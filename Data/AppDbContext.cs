91584db5cb4ee8d09bc4e74be5ed2d6a166bf9a2
CastionDev <jorgepocaderrimo.com@gmail.com>
2026-04-27 20:30:23 -0300

    feat: criação do model Product e DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudFronteiraTec.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudFronteiraTec.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudFronteiraTec.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [MaxLength(200)]
+        public string Descricao { get; set; }
+
+        [Range(0.01, 999999)]
+        public decimal Preco { get; set; }
+
+        public int Quantidade { get; set; }
+    }
+}
